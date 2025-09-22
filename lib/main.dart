955a158f6ef5dc305f96e415699435dba14a491e
ribeiro <rafael.ribeiro@evoluum.com.br>
2025-09-22 20:28:28 -0300

    paginas de cadastro

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,11 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:minhaserigrafia/modules/signup/ui/complete_sign_up_page.dart';
+import 'package:minhaserigrafia/modules/signin/ui/login_page.dart';
 import 'package:minhaserigrafia/shared/theme/default_theme.dart';
 
-
 import 'modules/app/app_module.dart';
-import 'modules/signin/ui/login_page.dart';
+import 'modules/signup/ui/create_password_page.dart';
+import 'modules/signup/ui/sign_up_page.dart';
 
 void main() {
   runApp(ModularApp(module: AppModule(), child: MyApp()));
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Minha Serigrafia',
       theme: defaultTheme,
-      home: const CompleteSignInPage(),
+      home: const LoginPage(),
     );
   }
 }
