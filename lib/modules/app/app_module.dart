c87958ed580114d5a5ec7c097f1e1318e1a3fe9e
ribeiro <rafael.ribeiro@evoluum.com.br>
2025-10-08 20:13:26 -0300

    remove unused import

--- lib/modules/app/app_module.dart
+++ lib/modules/app/app_module.dart
@@ -1,15 +1,13 @@
 import 'package:flutter_modular/flutter_modular.dart';
-import 'package:minhaserigrafia/modules/auth/auth_route_navigator.dart';
+import 'package:minhaserigrafia/modules/auth/auth_module.dart';
 import 'package:minhaserigrafia/modules/home/home_module.dart';
 import 'package:minhaserigrafia/modules/signin/login_module.dart';
 import 'package:minhaserigrafia/modules/signup/sign_up_module.dart';
-import 'package:minhaserigrafia/modules/auth/auth_module.dart';
 import 'package:minhaserigrafia/shared/routes/route_named.dart';
 
 class AppModule extends Module {
   @override
-  void binds(i) {
-  }
+  void binds(i) {}
 
   @override
   void routes(r) {
